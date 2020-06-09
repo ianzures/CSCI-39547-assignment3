13a552bdae2e156391426b6b3c3c1df3d9b89209
Jeremy <jeremy.gonzalez1128@gmail.com>
2020-06-09 13:22:10 -0400

    fixed error in remove column function closes #14

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-
 let numRows = 0;
 let numCols = 0;
+let color;
 
 
 function addR(){
@@ -17,71 +17,108 @@ function addR(){
             row.appendChild(column);
         }
     }
-	
+
     table.appendChild(row);
-    
+
     numRows++;
+
 }
 
 
 function addC(){
 
     let table = document.getElementById('grid');
-    let column = document.createElement('td');
     let row = document.createElement('tr');
 
+
     if(numRows === 0){
+
+         let column = document.createElement('td');
+       
+
         row.appendChild(column);
         table.appendChild(row);
         numRows++;
         numCols++;
+
     }
     else{
         let rows = document.getElementsByTagName('tr');
         for(let row of rows){
+            let column = document.createElement('td');
             row.appendChild(column);
         }
         numCols++;
+
     }
+
+
+
 }
 
 function removeR(){
+    if(numRows === 0 ){
+        alert("No rows to left remove.");
+        return;
+    }
+    let table = document.getElementById('grid');
+    let lastRow = table.lastElementChild;
+    /*for(let i = 0; i < numCols; i++){
+        let cell = lastRow.childNodes[0];
+        lastRow.removeChild(cell);
+    }*/
+
+    table.removeChild(lastRow);
+    numRows--;
+    if(numRows === 0){
+        numCols = 0;
+    }
+    console.log(numRows, numCols);
+
 
 }
 
 function removeC(){
-	let table = document.getElementById('grid');
-	let rows = document.getElementsByTagName('tr');
-	
-	if(numCols !== 0){
-		for(let r of rows){
-			let x = r.LastElementChild;
-			r.removeChild(x);
-		}
-		numCols--;	
-	}
-	
-	if(numCols === 0){
-             numRows = 0;
-	     table.innerHTML = "";
-	}
 
+    let table = document.getElementById('grid');
+    let rows = document.getElementsByTagName('tr');
+
+    if(numCols !== 0){
+        for(let r of rows){
+           let x = r.lastElementChild;
+            r.removeChild(x);
+
+        }
+        numCols--;
+
+    }
+    if(numCols === 0 ){
+        numRows = 0;
+        table.innerHTML = "";
+
+    }
 
 }
 
 function fillU(){
 
+  
+
 }
 
 function fill(){
+
+  
 
 }
 
 function clearAll(){
 
+
 }
 
 function selected(){
-	color = document.getElementById('selectedID').value;
-}
 
+    color = document.getElementById('selectedID').value;
+
+}

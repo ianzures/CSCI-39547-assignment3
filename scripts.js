69d516a7d05f81a8f833ee2b49189246434fa7f7
ianzures <i.anzures514@gmail.com>
2020-06-09 18:16:02 -0400

    Closes #22

--- scripts.js
+++ scripts.js
@@ -1,25 +1,31 @@
 
 let numRows = 0;
 let numCols = 0;
-
+let color;
 
 function addR(){
     let table = document.getElementById('grid');
     let row = document.createElement('tr');
     let column = document.createElement('td');
     if(numCols === 0){
+    column.addEventListener("click", function(){
+            this.style.backgroundColor = color;
+        })
         row.appendChild(column);
         numCols++;
     }
     else{
         for(let i = 0; i < numCols; i++){
             column = document.createElement('td');
+        column.addEventListener("click", function(){
+                this.style.backgroundColor = color;
+            })
             row.appendChild(column);
         }
     }
-	
+
     table.appendChild(row);
-    
+
     numRows++;
 }
 
@@ -27,64 +33,105 @@ function addR(){
 function addC(){
 
     let table = document.getElementById('grid');
-    let column = document.createElement('td');
     let row = document.createElement('tr');
 
+
     if(numRows === 0){
+
+         let column = document.createElement('td');
+         column.addEventListener("click", function(){
+            this.style.backgroundColor = color;
+         })
+
         row.appendChild(column);
         table.appendChild(row);
         numRows++;
         numCols++;
+
     }
     else{
         let rows = document.getElementsByTagName('tr');
         for(let row of rows){
+            let column = document.createElement('td');
+            column.addEventListener("click", function(){
+            this.style.backgroundColor = color;
+           })
             row.appendChild(column);
         }
         numCols++;
+
     }
 }
 
 function removeR(){
-    if(numRows === 0 ){
+     if(numRows === 0 ){
         alert("No rows to left remove.");
         return;
     }
     let table = document.getElementById('grid');
     let lastRow = table.lastElementChild;
-    /*for(let i = 0; i < numCols; i++){
-        let cell = lastRow.childNodes[0];
-        lastRow.removeChild(cell);
-    }*/
 
     table.removeChild(lastRow);
     numRows--;
+    if(numRows === 0){
+        numCols = 0;
+    }
+    console.log(numRows, numCols);
 }
 
 function removeC(){
-	let rows = document.getElementsByTagName('tr');
-	
-	if(numCols !== 0){
-		for(let r of rows){
-			let x = r.LastElementChild;
-			r.removeChild(x);
-		}
-		numCols--;	
-	}
+	let table = document.getElementById('grid');
+    let rows = document.getElementsByTagName('tr');
 
+    if(numCols !== 0){
+        for(let r of rows){
+           let x = r.lastElementChild;
+            r.removeChild(x);
+
+        }
+        numCols--;
+
+    }
+    if(numCols === 0 ){
+        numRows = 0;
+        table.innerHTML = "";
+        alert("No columns left to remove.")
+    }
 
 }
 
 function fillU(){
 
+ let table = document.getElementById('grid');
+
+    for(let i=0; i<table.rows.length; i++){
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            if(table.rows[i].cells[j].style.backgroundColor === '') {
+                table.rows[i].cells[j].style.backgroundColor = color;
+            }
+        }
+    }
 }
 
 function fill(){
 
+ let table = document.getElementById('grid');
+
+    for(let i=0; i<table.rows.length; i++){
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            table.rows[i].cells[j].style.backgroundColor = color;
+        }
+    }
 }
 
 function clearAll(){
+     let table = document.getElementById('grid');
 
+    for(let i=0; i<table.rows.length; i++){
+        for(let j=0; j<table.rows[i].cells.length; j++){
+            table.rows[i].cells[j].style.backgroundColor = "white";
+        }
+    }
 }
 
 function selected(){

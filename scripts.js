24b73fe9338717806af4a451771c28adb1957596
ianzures <i.anzures514@gmail.com>
2020-06-09 18:20:50 -0400

    Prints numRows and numCols after every row/column addition or deletion.

--- scripts.js
+++ scripts.js
@@ -7,8 +7,9 @@ function addR(){
     let table = document.getElementById('grid');
     let row = document.createElement('tr');
     let column = document.createElement('td');
+
     if(numCols === 0){
-    column.addEventListener("click", function(){
+        column.addEventListener("click", function(){
             this.style.backgroundColor = color;
         })
         row.appendChild(column);
@@ -17,7 +18,7 @@ function addR(){
     else{
         for(let i = 0; i < numCols; i++){
             column = document.createElement('td');
-        column.addEventListener("click", function(){
+            column.addEventListener("click", function(){
                 this.style.backgroundColor = color;
             })
             row.appendChild(column);
@@ -25,42 +26,39 @@ function addR(){
     }
 
     table.appendChild(row);
-
     numRows++;
+    console.log(numRows, numCols);
 }
 
 
 function addC(){
-
     let table = document.getElementById('grid');
     let row = document.createElement('tr');
 
-
     if(numRows === 0){
-
-         let column = document.createElement('td');
-         column.addEventListener("click", function(){
+        let column = document.createElement('td');
+        column.addEventListener("click", function(){
             this.style.backgroundColor = color;
-         })
+        })
 
         row.appendChild(column);
         table.appendChild(row);
         numRows++;
         numCols++;
-
     }
     else{
         let rows = document.getElementsByTagName('tr');
         for(let row of rows){
             let column = document.createElement('td');
             column.addEventListener("click", function(){
-            this.style.backgroundColor = color;
-           })
+                this.style.backgroundColor = color;
+            })
             row.appendChild(column);
         }
         numCols++;
-
     }
+
+    console.log(numRows, numCols);
 }
 
 function removeR(){
@@ -68,14 +66,17 @@ function removeR(){
         alert("No rows to left remove.");
         return;
     }
+
     let table = document.getElementById('grid');
     let lastRow = table.lastElementChild;
 
     table.removeChild(lastRow);
     numRows--;
+
     if(numRows === 0){
         numCols = 0;
     }
+
     console.log(numRows, numCols);
 }
 
@@ -85,24 +86,23 @@ function removeC(){
 
     if(numCols !== 0){
         for(let r of rows){
-           let x = r.lastElementChild;
+            let x = r.lastElementChild;
             r.removeChild(x);
-
         }
         numCols--;
-
     }
-    if(numCols === 0 ){
+
+    if(numCols === 0){
         numRows = 0;
         table.innerHTML = "";
         alert("No columns left to remove.")
     }
 
+    console.log(numRows, numCols);
 }
 
 function fillU(){
-
- let table = document.getElementById('grid');
+    let table = document.getElementById('grid');
 
     for(let i=0; i<table.rows.length; i++){
         for(let j=0; j<table.rows[i].cells.length; j++){
@@ -114,8 +114,7 @@ function fillU(){
 }
 
 function fill(){
-
- let table = document.getElementById('grid');
+    let table = document.getElementById('grid');
 
     for(let i=0; i<table.rows.length; i++){
         for(let j=0; j<table.rows[i].cells.length; j++){
@@ -125,7 +124,7 @@ function fill(){
 }
 
 function clearAll(){
-     let table = document.getElementById('grid');
+    let table = document.getElementById('grid');
 
     for(let i=0; i<table.rows.length; i++){
         for(let j=0; j<table.rows[i].cells.length; j++){
